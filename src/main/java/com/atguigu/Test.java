05a272a80811d8409648d091bba786c126c17650
zhangping <731089542@qq.com>
2021-01-28 18:11:18 +0800

    首次提交

---
+++ src/main/java/com/atguigu/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 版权所有©咪咕音乐 All rights reserved
+ **/
+package com.atguigu;
+
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println("第一个maven工程");
+  }
+
+}
