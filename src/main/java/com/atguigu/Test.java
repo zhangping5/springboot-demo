8739d2e95b11fc1dc1680894f1c3e9a6a122f788
zhangping5 <44861456+zhangping5@users.noreply.github.com>
2021-02-01 11:20:27 +0800

    Update Test.java

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -6,7 +6,7 @@ package com.atguigu;
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("第一个maven工程");
+    System.out.println("第一个maven工程,我对它进行修改啦");
   }
 
 }
