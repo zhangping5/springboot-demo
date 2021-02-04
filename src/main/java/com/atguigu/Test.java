39175818fbf0be3c853807933be17d35fd78c988
zhangping <731089542@qq.com>
2021-02-04 09:49:49 +0800

    增加1111内容

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -8,6 +8,7 @@ public class Test {
   public static void main(String[] args) {
     String branch = "我是dev分支啊";
     System.out.println("第一个maven工程,我对它进行修改啦");
+    System.out.println("1111");
   }
 
 }
